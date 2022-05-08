bcecb942747376a8db575f7a6c7a749242d3d2ed
Thomas Breydo <tbreydo@gmail.com>
2022-05-08 12:32:27 -0400

    Set up Token enums (Keyword, Literal, Punctuation)

--- src/tokenization/token.rs
+++ src/tokenization/token.rs
@@ -1,12 +1,64 @@
 pub enum Token {
-    EOF,
-
-    // commands
-    Func,
-    Import,
-
-    // primary
+    Keyword(Keyword),
     Identifier(String),
-    Number(f64)
+    Literal(Literal),
+    Punctuation(Punctuation),
 }
 
+pub enum Keyword {
+    Int,
+    Float,
+    Str,
+    Bool,
+    Map,
+    Arr,
+    Set,
+
+    Or,
+    And,
+    Not,
+    True,
+    False,
+
+    If,
+    Fn,
+    While,
+    For,
+}
+
+pub enum Literal {
+    Float(f64),
+    Int(isize),
+    Str(String),
+}
+
+pub enum Punctuation {
+    Ampersand,
+    Asterisk,
+    At,
+    Caret,
+    Dash,
+    Dollar,
+    Dot,
+    DoubleQuote,
+    Exclamation,
+    Hashtag,
+    Percent,
+    Pipe,
+    Plus,
+    Question,
+    SingleQuote,
+    Tilde,
+
+    OpenBracket(Bracket),
+    CloseBracket(Bracket),
+
+    Unknown(char),
+}
+
+pub enum Bracket {
+    Round,
+    Square,
+    Curly,
+    Angle,
+}
