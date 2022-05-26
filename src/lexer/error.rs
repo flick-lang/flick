8bb6b5651ad52ed8af96c09b0a682efd5290e021
Max Larsson <maxslarsson@gmail.com>
2022-05-26 02:57:07 -0400

    Improve error handling and printing

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -7,9 +7,8 @@ pub enum Error {
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Error: ")?;
         match self {
-            Self::UnknownChar(c) => writeln!(f, "unknown char '{}'", c),
+            Self::UnknownChar(c) => write!(f, "unknown char '{}'", c),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
     unused_lifetimes
 )]
 use crate::program::Program;
+use owo_colors::{OwoColorize, Stream::Stderr};
+use std::error::Error;
 
 mod lexer;
 mod program;
@@ -12,6 +14,32 @@ mod program;
 fn main() {
     let program = Program::from_file("examples/bad.fl");
     for token in program.tokens() {
-        println!("{:?}", token.unwrap()); // should panic at ≈
+        match token {
+            Ok(t) => println!("{:?}", t), // should panic at ≈
+            Err(e) => handle_error(e),
+        }
     }
+}
+
+fn handle_error(err: impl Error) {
+    // TODO: Print line and column numbers of error
+    eprintln!(
+        "{} {}",
+        "Error: ".if_supports_color(Stderr, |text| text.red()),
+        err
+    );
+
+    // NOTE: These APIs aren't stable yet, but they should be used once they are
+    // let boxed_err: Box<dyn Error> = err.into();
+    // for e in boxed_err.chain() {
+    //     eprintln!("caused by: {}", e);
+    // }
+
+    // // The backtrace is not always generated
+    // // Run with 'RUST_BACKTRACE=1'
+    // if let Some(backtrace) = err.backtrace() {
+    //     eprintln!("backtrace: {:?}", backtrace);
+    // }
+
+    std::process::exit(1);
 }
