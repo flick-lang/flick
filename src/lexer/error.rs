8d54352a46f778ccbe11ba0b559e5dfda1ba87a9
Max Larsson <maxslarsson@gmail.com>
2022-05-26 16:58:40 -0400

    Print unicode when printing UnknownChar error

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -30,7 +30,7 @@ impl Display for ErrorKind {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::FloatParsing(float) => write!(f, "invalid float literal '{}'", float),
-            Self::UnknownChar(c) => write!(f, "unknown char '{}'", c),
+            Self::UnknownChar(c) => write!(f, "unknown char '{}' (U+{:x})", c, *c as u32),
         }
     }
 }
