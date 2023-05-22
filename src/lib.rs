0df7917bea3e1c44610e58103c855caefa8759a4
Max Larsson <maxslarsson@gmail.com>
2023-05-22 00:14:04 -0400

    :boom: Starting over

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod lexer;
-pub mod source_file;
-
-pub use source_file::SourceFile;
-
-#[cfg(test)]
-const TEST_FILE_PATH: &str = "/home/test.fl";
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use flick::SourceFile;
-
 fn main() {
-    let program = SourceFile::from_file("examples/bad_strs.fl").expect("error reading file");
-    for token in program.tokens() {
-        match token {
-            Ok(t) => println!("{:?}", t),
-            Err(e) => eprintln!("{}", e),
-        }
-    }
+    println!("Hello, world!");
 }
