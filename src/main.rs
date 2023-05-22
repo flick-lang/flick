035e3f3ff8abc7d04cc26a53beb8c7af6f7ed9a1
Max Larsson <maxslarsson@gmail.com>
2023-05-22 00:47:34 -0400

    Create initial CLI and read input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    file_path: PathBuf,
 }
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let file = File::open(cli.file_path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut file_contents = String::new();
+    buf_reader.read_to_string(&mut file_contents)?;
+
+    println!("File contents: \n{}", file_contents);
+
+    Ok(())
+}
+
