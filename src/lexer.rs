2d669229607ff9329333edad8da505aee4e7f44c
Thomas Breydo <tbreydo@gmail.com>
2023-05-23 14:58:50 -0400

    Refactor lexer and add tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,7 +15,7 @@ impl<'a> Lexer<'a> {
     }
 
     fn next_token(&mut self) -> Option<Token> {
-        self.skip_whitespace();
+        self.skip_non_newline_whitespace();
 
         // Figure out what type the next token is and call handling function
         let token = match self.chars.peek()? {
@@ -30,25 +30,29 @@ impl<'a> Lexer<'a> {
         Some(token)
     }
 
-    fn skip_whitespace(&mut self) {
-        while let Some(&c) = self.chars.peek() {
-            if c.is_whitespace() && c != '\n' {
-                self.chars.next();
+    fn take_chars_while(&mut self, predicate: impl Fn(&char) -> bool) -> String {
+        let mut s = String::new();
+        while let Some(c) = self.chars.peek() {
+            if predicate(c) {
+                s.push(self.chars.next().unwrap());
             } else {
-                break;
+                break
             }
         }
+        return s;
     }
 
-    fn read_word(&mut self) -> Token {
-        let mut s = String::new();
-        while let Some(&c) = self.chars.peek() {
-            if !c.is_ascii_alphanumeric() && c != '_' {
-                break;
-            }
-            s.push(self.chars.next().unwrap());
-        }
+    fn skip_chars_while(&mut self, predicate: impl Fn(&char) -> bool) {
+        let _ = self.take_chars_while(predicate);
+    }
 
+    fn skip_non_newline_whitespace(&mut self) {
+        self.skip_chars_while(|&c| c.is_whitespace() && c != '\n');
+    }
+
+    /// Assuming lexer peeked 'a'-'z', 'A'-'Z', or '_'.
+    fn read_word(&mut self) -> Token {
+        let s = self.take_chars_while(|&c| c.is_ascii_alphanumeric() || c == '_');
         match s.as_str() {
             "var" => Token::Var,
             "int" => Token::Int,
@@ -57,40 +61,22 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    /// Assuming lexer peeked a digit.
     fn read_usize_literal(&mut self) -> Token {
-        let mut number = String::new();
-
-        while let Some(c) = self.chars.peek() {
-            if !c.is_ascii_digit() {
-                break;
-            }
-            number.push(self.chars.next().unwrap());
-        }
-
+        let number = self.take_chars_while(|&c| c.is_ascii_digit());
         Token::Usize(number.parse().unwrap())
     }
 
+    /// Assuming lexer peeked a '#'.
     fn read_comment(&mut self) -> Token {
-        let mut comment = String::new();
+        self.skip_chars_while(|&c| c == '#');
+        self.skip_non_newline_whitespace();
 
-        while let Some(&c) = self.chars.peek() {
-            if c == '#' || c.is_whitespace() {
-                self.chars.next(); // ignore '#' and whitespace at start of comment
-            } else {
-                break;
-            }
-        }
-
-        while let Some(c) = self.chars.next() {
-            if c == '\n' {
-                break;
-            }
-            comment.push(c);
-        }
-
-        Token::Comment(comment)
+        Token::Comment(self.take_chars_while(|&c| c != '\n'))
     }
 
+    /// Assuming lexer peeked a character like '*' or '>' that might be proceeded
+    /// by an '='.
     fn read_operator(&mut self) -> Token {
         let mut operator = String::new();
         operator.push(self.chars.next().unwrap());
@@ -146,12 +132,58 @@ impl<'a> Iterator for Lexer<'a> {
 mod tests {
     use crate::token::Token;
     use crate::lexer::Lexer;
+    // todo make macro to avoid last three lines of boilerplate
 
     #[test]
     fn comments() {
-        let source_code = "#    simple comment";
+        let source_code = "#    simple comment\n######   ## complex comment";
         let expected_tokens = vec![
-            Token::Comment("simple comment".to_string())
+            Token::Comment("simple comment".to_string()),
+            Token::Newline,
+            Token::Comment("## complex comment".to_string()),
+        ];
+
+        let lexer = Lexer::new(source_code);
+        let received_tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(received_tokens, expected_tokens);
+    }
+
+    #[test]
+    fn variables() {
+        let source_code = "var this_is_a_LONG_VARIABLE_NAME: int = 5\nvar shortInt: int = 5";
+        let expected_tokens = vec![
+            Token::Var,
+            Token::Identifier("this_is_a_LONG_VARIABLE_NAME".to_string()),
+            Token::Colon,
+            Token::Int,
+            Token::Assign,
+            Token::Usize(5),
+            Token::Newline,
+            Token::Var,
+            Token::Identifier("shortInt".to_string()),
+            Token::Colon,
+            Token::Int,
+            Token::Assign,
+            Token::Usize(5),
+        ];
+
+        let lexer = Lexer::new(source_code);
+        let received_tokens: Vec<_> = lexer.collect();
+
+        assert_eq!(received_tokens, expected_tokens);
+    }
+
+    #[test]
+    fn while_loop() {
+        let source_code = "while x <= 5 {}";
+        let expected_tokens = vec![
+            Token::While,
+            Token::Identifier("x".to_string()),
+            Token::LEQ,
+            Token::Usize(5),
+            Token::LSquirly,
+            Token::RSquirly,
         ];
 
         let lexer = Lexer::new(source_code);
