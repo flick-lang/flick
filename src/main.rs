ac9b0951da090a36004511face7863397ba4bd30
Max Larsson <maxslarsson@gmail.com>
2023-11-21 19:34:29 -0500

    Use ld instead of clang to link

--- src/main.rs
+++ src/main.rs
@@ -92,7 +92,7 @@ fn main() -> Result<()> {
     compiler.to_file(&object_output_path);
 
     let executable_output_path = cli.get_executable_output_path();
-    Command::new("clang")
+    Command::new("ld")
         .arg(&object_output_path)
         .arg("-o")
         .arg(&executable_output_path)
