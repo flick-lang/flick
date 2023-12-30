a328b9511e8a064447f0b5d15f447555d814af58
Thomas Breydo <tbreydo@gmail.com>
2023-12-30 15:45:33 -0500

    Make highest-level module docs more detailed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![doc = include_str!("../README.md")]
 
-/// Converts [abstract syntax trees](parser::ast) into LLVM using llvm-sys
+/// Module to convert [abstract syntax trees](parser::ast) into LLVM using llvm-sys
+///
+/// The general idea is to take code objects (e.g. variables or functions) and form
+/// instances of [LLVMValueRef][a]. Then, after we're done, we ask llvm-sys to
+/// generate LLVM code.
+///
+/// [a]: llvm_sys::prelude::LLVMValueRef
 mod compiler;
-/// Converts source files into token streams
+/// Module to convert source files into token streams
 mod lexer;
-/// Converts token streams into [abstract syntax trees](parser::ast)
+/// Module to convert token streams into [abstract syntax trees](parser::ast)
 mod parser;
 
 use std::fs::File;
