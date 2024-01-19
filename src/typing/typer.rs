268cb0add92db41c83b74ca1a27252ebf8e2784c
Max Larsson <maxslarsson@gmail.com>
2024-01-19 02:16:00 -0500

    Add type inference!

--- src/typing/typer.rs
+++ src/typing/typer.rs
@@ -22,13 +22,18 @@ impl Typer {
 
     pub fn type_program(&mut self, program: &Program) -> TypedProgram {
         let mut func_defs = Vec::with_capacity(program.func_defs.len());
+
         for func_def in program.func_defs.iter() {
-            func_defs.push(self.type_func_def(func_def))
+            self.define_func(func_def);
         }
+        for func_def in program.func_defs.iter() {
+            func_defs.push(self.type_func_def(func_def));
+        }
+
         TypedProgram { func_defs }
     }
 
-    fn type_func_def(&mut self, func_def: &FuncDef) -> TypedFuncDef {
+    fn define_func(&mut self, func_def: &FuncDef) {
         let func_name = &func_def.proto.name;
         match self.scope_manager.get(func_name) {
             Some(Type::Func(_)) => panic!("Cannot redefine function '{}'", func_name),
@@ -51,7 +56,9 @@ impl Typer {
             return_type,
         });
         self.scope_manager.set(func_name, func_type);
+    }
 
+    fn type_func_def(&mut self, func_def: &FuncDef) -> TypedFuncDef {
         self.scope_manager.enter_scope();
 
         for param in func_def.proto.params.iter() {
@@ -62,7 +69,7 @@ impl Typer {
 
         let mut func_body = Vec::with_capacity(func_def.body.len());
         for statement in func_def.body.iter() {
-            func_body.push(self.type_statement(statement));
+            func_body.push(self.type_statement(statement, &func_def.proto.return_type));
         }
 
         self.scope_manager.exit_scope();
@@ -73,21 +80,29 @@ impl Typer {
         }
     }
 
-    fn type_statement(&mut self, statement: &Statement) -> TypedStatement {
+    fn type_statement(
+        &mut self,
+        statement: &Statement,
+        function_return_type: &Type,
+    ) -> TypedStatement {
         match statement {
             Statement::VarDeclaration(v) => {
                 TypedStatement::VarDeclaration(self.type_var_declaration(v))
             }
-            Statement::WhileLoop(w) => TypedStatement::WhileLoop(self.type_while_loop(w)),
+            Statement::WhileLoop(w) => {
+                TypedStatement::WhileLoop(self.type_while_loop(w, function_return_type))
+            }
             Statement::Assignment(a) => TypedStatement::Assignment(self.type_assignment(a)),
-            Statement::Return(r) => TypedStatement::Return(self.type_return(r.as_ref())),
+            Statement::Return(r) => {
+                TypedStatement::Return(self.type_return(r.as_ref(), function_return_type))
+            }
         }
     }
 
     fn type_var_declaration(&mut self, var_declaration: &VarDeclaration) -> TypedVarDeclaration {
         let var_name = var_declaration.var_name.clone();
         let var_type = var_declaration.var_type.clone();
-        let var_value = self.type_expr(&var_declaration.var_value);
+        let var_value = self.type_expr(&var_declaration.var_value, Some(&var_type));
         self.scope_manager.set(&var_name, var_type.clone());
 
         TypedVarDeclaration {
@@ -97,14 +112,19 @@ impl Typer {
         }
     }
 
-    fn type_while_loop(&mut self, while_loop: &WhileLoop) -> TypedWhileLoop {
-        let condition = self.type_expr(&while_loop.condition);
+    fn type_while_loop(
+        &mut self,
+        while_loop: &WhileLoop,
+        function_return_type: &Type,
+    ) -> TypedWhileLoop {
+        let bool_type = Type::Int(IntType { width: 1 });
+        let condition = self.type_expr(&while_loop.condition, Some(&bool_type));
 
         self.scope_manager.enter_scope();
         let body: Vec<_> = while_loop
             .body
             .iter()
-            .map(|s| self.type_statement(s))
+            .map(|s| self.type_statement(s, function_return_type))
             .collect();
         self.scope_manager.exit_scope();
 
@@ -113,39 +133,59 @@ impl Typer {
 
     fn type_assignment(&mut self, assignment: &Assignment) -> TypedAssignment {
         let name = assignment.name.clone();
-        let value = self.type_expr(assignment.value.as_ref());
-        // self.scope_manager.set(&name, value.expr_type.clone());
+        let var_type = match self.scope_manager.get(&name) {
+            Some(t) => t.clone(),
+            None => panic!("Variable '{}' has not been declared yet", name),
+        };
+        let value = self.type_expr(assignment.value.as_ref(), Some(&var_type));
+
+        self.scope_manager.set(&name, var_type);
+
         TypedAssignment {
             name,
             value: Box::new(value),
         }
     }
 
-    fn type_return(&mut self, ret: Option<&Expr>) -> Option<TypedExpr> {
-        ret.map(|e| self.type_expr(e))
+    fn type_return(
+        &mut self,
+        ret: Option<&Expr>,
+        function_return_type: &Type,
+    ) -> Option<TypedExpr> {
+        ret.map(|e| self.type_expr(e, Some(&function_return_type)))
     }
 
-    fn type_expr(&mut self, expr: &Expr) -> TypedExpr {
+    fn type_expr(&mut self, expr: &Expr, desired_type: Option<&Type>) -> TypedExpr {
         match expr {
             Expr::Identifier(id) => TypedExpr::Identifier(id.clone()),
-            Expr::IntLiteral(int) => TypedExpr::IntLiteral(self.type_int_literal(int)),
-            Expr::Binary(b) => TypedExpr::Binary(self.type_bin_expr(b)),
-            Expr::Comparison(c) => TypedExpr::Comparison(self.type_comparison_expr(c)),
-            Expr::Call(c) => TypedExpr::Call(self.type_call_expr(c)),
+            Expr::IntLiteral(int) => {
+                TypedExpr::IntLiteral(self.type_int_literal(int, desired_type))
+            }
+            Expr::Binary(b) => TypedExpr::Binary(self.type_bin_expr(b, desired_type)),
+            Expr::Comparison(c) => {
+                TypedExpr::Comparison(self.type_comparison_expr(c, desired_type))
+            }
+            Expr::Call(c) => TypedExpr::Call(self.type_call_expr(c, desired_type)),
         }
     }
 
-    fn type_int_literal(&mut self, int_literal: &str) -> TypedIntLiteral {
+    fn type_int_literal(
+        &mut self,
+        int_literal: &str,
+        desired_type: Option<&Type>,
+    ) -> TypedIntLiteral {
+        let default_type = Type::Int(IntType { width: 64 });
+
         TypedIntLiteral {
             int_value: int_literal.to_string(),
-            int_type: Type::Int(IntType { width: 64 }),
+            int_type: desired_type.unwrap_or(&default_type).clone(),
         }
     }
 
-    fn type_bin_expr(&mut self, bin_expr: &Binary) -> TypedBinary {
-        let left = self.type_expr(&bin_expr.left);
+    fn type_bin_expr(&mut self, bin_expr: &Binary, desired_type: Option<&Type>) -> TypedBinary {
+        let left = self.type_expr(&bin_expr.left, desired_type);
         let operator = bin_expr.operator;
-        let right = self.type_expr(&bin_expr.right);
+        let right = self.type_expr(&bin_expr.right, desired_type);
         TypedBinary {
             left: Box::new(left),
             operator,
@@ -153,10 +193,20 @@ impl Typer {
         }
     }
 
-    fn type_comparison_expr(&mut self, comparison: &Comparison) -> TypedComparison {
-        let left = self.type_expr(&comparison.left);
+    fn type_comparison_expr(
+        &mut self,
+        comparison: &Comparison,
+        desired_type: Option<&Type>,
+    ) -> TypedComparison {
+        if desired_type != Some(&Type::Int(IntType { width: 1 })) {
+            panic!("Comparison returns an i1 but expected '{:?}'", desired_type);
+        }
+        // TODO: Find common type
+
+        let left = self.type_expr(&comparison.left, None);
         let operator = comparison.operator;
-        let right = self.type_expr(&comparison.right);
+        let right = self.type_expr(&comparison.right, None);
+
         TypedComparison {
             left: Box::new(left),
             operator,
@@ -164,12 +214,39 @@ impl Typer {
         }
     }
 
-    fn type_call_expr(&mut self, call_expr: &Call) -> TypedCall {
+    fn type_call_expr(&mut self, call_expr: &Call, desired_type: Option<&Type>) -> TypedCall {
         let function_name = call_expr.function_name.clone();
-        let args: Vec<_> = call_expr.args.iter().map(|a| self.type_expr(a)).collect();
+
+        let function_type = match self.scope_manager.get(&function_name) {
+            Some(Type::Func(f)) => f.clone(),
+            Some(_) => panic!("Variable '{}' is not a function", function_name),
+            None => panic!(
+                "Function '{}' is called but has not been defined",
+                function_name
+            ),
+        };
+
+        if Some(function_type.return_type.as_ref()) != desired_type {
+            panic!(
+                "Expected function '{}' to return '{}' but it has return type '{:?}'",
+                function_name, function_type.return_type, desired_type
+            )
+        }
+
+        let args: Vec<_> = call_expr
+            .args
+            .iter()
+            .zip(function_type.param_types.iter())
+            .map(|(e, t)| self.type_expr(e, Some(t)))
+            .collect();
+
         TypedCall {
             function_name,
             args,
         }
     }
+
+    fn cast_type() {}
+
+    fn find_common_type() {}
 }
